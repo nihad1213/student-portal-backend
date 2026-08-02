715aaa72a1bb3c9bbcd0c8c1cde016ef56c74895
Nihad <nihad.nemetli@gmail.com>
2026-08-02 13:13:47 +0400

    first commit

---
+++ src/test/java/com/spb/studentportalbackend/StudentPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spb.studentportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
