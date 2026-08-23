1c98f486e2f7c2dc008338a4cb541dba47284b6e
Nihad <nihad.nemetli@gmail.com>
2026-08-23 17:54:27 +0400

    add: BaseEntity fields added

--- src/main/java/com/spb/studentportalbackend/entity/BaseEntity.java
+++ src/main/java/com/spb/studentportalbackend/entity/BaseEntity.java
@@ -1,5 +1,27 @@
 package com.spb.studentportalbackend.entity;
 
-public abstract class BaseEntity {
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
