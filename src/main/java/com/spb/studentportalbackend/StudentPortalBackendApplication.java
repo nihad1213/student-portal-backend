780705263c63370e66733345102f7c0589121955
Nihad <nihad.nemetli@gmail.com>
2026-08-23 18:27:08 +0400

    add: Enttiy scan added

--- src/main/java/com/spb/studentportalbackend/StudentPortalBackendApplication.java
+++ src/main/java/com/spb/studentportalbackend/StudentPortalBackendApplication.java
@@ -2,10 +2,12 @@ package com.spb.studentportalbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = "com.spb.studentportalbackend.entity" )
 public class StudentPortalBackendApplication {
 
     public static void main(String[] args) {
