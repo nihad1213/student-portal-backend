7b6787de31526716b371fbc28a356591432bd275
Nihad <nihad.nemetli@gmail.com>
2026-08-23 18:23:56 +0400

    new: libraries updated, BaseEntity fully created

--- src/main/java/com/spb/studentportalbackend/entity/BaseEntity.java
+++ src/main/java/com/spb/studentportalbackend/entity/BaseEntity.java
@@ -2,13 +2,17 @@ package com.spb.studentportalbackend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
