8f69398e1cd7adca9696afaadcc43abdd1304f78
Nihad <nihad.nemetli@gmail.com>
2026-08-30 03:25:47 +0400

    add: CreateUserRequest fields added

--- src/main/java/com/spb/studentportalbackend/dto/user/request/CreateUserRequest.java
+++ src/main/java/com/spb/studentportalbackend/dto/user/request/CreateUserRequest.java
@@ -1,5 +1,7 @@
 package com.spb.studentportalbackend.dto.user.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +11,25 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateUserRequest {
+    @NotBlank
+    String username;
 
+    @NotBlank
+    String firstName;
+
+    @NotBlank
+    String lastName;
+
+    @NotBlank
+    String phoneNumber;
+
+    @NotBlank
+    @Email
+    String mail;
+
+    @NotBlank
+    String password;
+
+    @NotBlank
+    String role;
 }
