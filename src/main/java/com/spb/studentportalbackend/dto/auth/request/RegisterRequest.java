cef3468f8bbc7ac14d6c17871c60dc243aed4ae6
Nihad <nihad.nemetli@gmail.com>
2026-08-30 02:57:02 +0400

    upd: RegisterRequest fields added

--- src/main/java/com/spb/studentportalbackend/dto/auth/request/RegisterRequest.java
+++ src/main/java/com/spb/studentportalbackend/dto/auth/request/RegisterRequest.java
@@ -1,4 +1,32 @@
 package com.spb.studentportalbackend.dto.auth.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegisterRequest {
+    @NotBlank
+    String username;
+
+    @NotBlank
+    String firstName;
+
+    @NotBlank
+    String lastName;
+
+    @NotBlank
+    String phoneNumber;
+
+    @NotBlank
+    @Email
+    String mail;
+
+    @NotBlank
+    String password;
 }
