c9fc77ed0ee50b4a943817e709d0d802e8294bf1
Nihad <nihad.nemetli@gmail.com>
2026-08-30 03:22:00 +0400

    new: user CreateRequest created

---
+++ src/main/java/com/spb/studentportalbackend/dto/user/request/CreateUserRequest.java
@@ -0,0 +1,13 @@
+package com.spb.studentportalbackend.dto.user.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateUserRequest {
+
+}
