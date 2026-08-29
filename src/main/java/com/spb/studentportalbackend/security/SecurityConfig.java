bce5972f3c7b2585b1e1822bfb6b70ccad6d08f6
Nihad <nihad.nemetli@gmail.com>
2026-08-30 03:48:15 +0400

    add: endpoint permission added

--- src/main/java/com/spb/studentportalbackend/security/SecurityConfig.java
+++ src/main/java/com/spb/studentportalbackend/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/user/create").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
