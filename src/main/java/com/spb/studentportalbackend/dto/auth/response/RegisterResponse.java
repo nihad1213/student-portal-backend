31652c162da82ba148f8c5d12a0bab65e876989d
Nihad <nihad.nemetli@gmail.com>
2026-08-30 02:57:12 +0400

    upd: RegisteResponse fields added

--- src/main/java/com/spb/studentportalbackend/dto/auth/response/RegisterResponse.java
+++ src/main/java/com/spb/studentportalbackend/dto/auth/response/RegisterResponse.java
@@ -1,4 +1,17 @@
 package com.spb.studentportalbackend.dto.auth.response;
 
+import com.spb.studentportalbackend.common.RoleEnum;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegisterResponse {
+    Long id;
+    String username;
+    String mail;
+    RoleEnum role;
 }
