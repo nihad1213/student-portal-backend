9c40ef103c5d1a0be51ebb8a411a6f022a97274c
Nihad <nihad.nemetli@gmail.com>
2026-08-30 03:24:28 +0400

    new: CreateUserService created

---
+++ src/main/java/com/spb/studentportalbackend/service/user/CreateUserService.java
@@ -0,0 +1,12 @@
+package com.spb.studentportalbackend.service.user;
+
+import com.spb.studentportalbackend.dto.user.request.CreateUserRequest;
+import com.spb.studentportalbackend.dto.user.response.CreateUserResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateUserService {
+    public CreateUserResponse create(CreateUserRequest request) {
+
+    }
+}
