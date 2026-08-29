0dde0fe4590c545d2054ab86e1cf34b2d736957f
Nihad <nihad.nemetli@gmail.com>
2026-08-30 03:22:47 +0400

    new: CreateUserResponse created

---
+++ src/main/java/com/spb/studentportalbackend/dto/user/response/CreateUserResponse.java
@@ -0,0 +1,13 @@
+package com.spb.studentportalbackend.dto.user.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateUserResponse {
+
+}
