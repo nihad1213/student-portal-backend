cb8a06fbf60c6fd1f412274e994a38fdf8a2d3f6
Nihad <nihad.nemetli@gmail.com>
2026-08-30 02:42:12 +0400

    new: UserRepository created

---
+++ src/main/java/com/spb/studentportalbackend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.spb.studentportalbackend.repository;
+
+import com.spb.studentportalbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByMail(String mail);
+}
