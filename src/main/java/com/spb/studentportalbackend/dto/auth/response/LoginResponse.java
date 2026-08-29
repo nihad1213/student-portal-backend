4667ee3a4f128b08895f95e016c8d1a9b7b074d6
Nihad <nihad.nemetli@gmail.com>
2026-08-30 02:53:13 +0400

    upd: Field defaults added

--- src/main/java/com/spb/studentportalbackend/dto/auth/response/LoginResponse.java
+++ src/main/java/com/spb/studentportalbackend/dto/auth/response/LoginResponse.java
@@ -1,11 +1,13 @@
 package com.spb.studentportalbackend.dto.auth.response;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginResponse {
-
-    private String token;
+    String token;
 }
